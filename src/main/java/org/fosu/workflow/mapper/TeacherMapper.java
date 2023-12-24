f409f7a72bc0a6de1da16342e539faf0a54c11da
chaoxin <1554018240@qq.com>
2023-12-25 01:38:44 +0800

    新建课程通知的时候，自动插入用户的姓名到Teacher_nick_name，例如zhangsan用户新建课程通知，自动将‘张三’插入Teacher_nick_name

--- src/main/java/org/fosu/workflow/mapper/TeacherMapper.java
+++ src/main/java/org/fosu/workflow/mapper/TeacherMapper.java
@@ -2,6 +2,11 @@ package org.fosu.workflow.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.fosu.workflow.entities.Teacher;
+import org.fosu.workflow.entities.courseManager;
+
+import java.util.ArrayList;
 
 public interface TeacherMapper extends BaseMapper<Teacher> {
+    Teacher getNickNameByTeacherName(String name);
+
 }
